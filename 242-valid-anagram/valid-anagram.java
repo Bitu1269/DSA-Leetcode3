acfe51db569cce4d97ab0b46c2966e4037d896d6
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-02-22 17:07:51 +0530

    Time: 4 ms (80.40%) | Memory: 46.6 MB (33.54%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,36 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // if(s.length() != t.length()) return false;
+
+        // int[] count = new int[26];
+
+        // for(int i=0; i<s.length(); i++){
+        //     count[s.charAt(i) - 'a']++;
+        // }
+        // for(int i=0; i<t.length(); i++){
+        //     count[t.charAt(i) - 'a']--;
+        // }
+        // for(int i=0; i<count.length; i++){
+        //     if(count[i] 
+        //     != 0) return false;
+        // }
+        // return true;
         if(s.length() != t.length()) return false;
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
 
-        int[] count = new int[26];
+        Arrays.sort(str1);
+        Arrays.sort(str2);
 
-        for(int i=0; i<s.length(); i++){
-            count[s.charAt(i) - 'a']++;
+        int i = 0;
+        while (i < str1.length) {
+            if (str1[i] != str2[i]) {
+                return false;
+            }
+            i++;
         }
-        for(int i=0; i<t.length(); i++){
-            count[t.charAt(i) - 'a']--;
-        }
-        for(int i=0; i<count.length; i++){
-            if(count[i] 
-            != 0) return false;
-        }
+
         return true;
+
     }
 }
